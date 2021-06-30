70e43ebd0f4eff6c11e36846e21752caecc7869c
Lydia <lydia.lucille.yeager@gmail.com>
2021-06-29 22:10:58 -0400

    working vue and html

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!--<img alt="Vue logo" src="./assets/logo.png" />-->
+  <!--<HelloWorld msg="Hello Vue 3 + Vite" />-->
+  <!--<KeyPad />-->
+  <RebirthLuggage />
+</template>
+
+<script setup>
+//import HelloWorld from './components/HelloWorld.vue'
+import KeyPad from './components/KeyPad.vue'
+import RebirthLuggage from './components/RebirthLuggage.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/KeyPad.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="keypad">
+      <!--<input v-model='code' class="keyCode" type='text' placeholder='________' /> -->
+
+      <div class="keyCodeContainer">
+        <div class="keyCode">
+          {{ code }}
+        </div>
+      </div>
+    
+      <table class="keyboard" cellSpacing="10px">
+        <tbody align="center" class="keyboard-body">
+          <tr>
+            <td><button class="key numberKey" @click="onKeyPress('1')">1</button></td>
+            <td><button class="key numberKey">2</button></td>
+            <td><button class="key numberKey">3</button></td>
+            <td rowspan="3"><button class="key submitKey" >Submit</button></td>
+          </tr>
+          <tr>
+            <td><button class="key numberKey">4</button></td>
+            <td><button class="key numberKey">5</button></td>
+            <td><button class="key numberKey">6</button></td>
+          </tr>
+          <tr>
+            <td><button class="key numberKey">7</button></td>
+            <td><button class="key numberKey">8</button></td>
+            <td><button class="key numberKey">9</button></td>
+          </tr>
+          <tr>
+            <td><button class="key numberKey">&lt;</button></td>
+            <td><button class="key numberKey">0</button></td>
+            <td><button class="key numberKey">-</button></td>
+            <td><button class="key resetKey">Reset</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import './keypad.css'
+import {defineProps, reactive } from 'vue'
+
+    const onKeyPress = (value) => {
+        if (code === undefined || code.value === null) {
+            code = ''
+        }
+
+        code += value
+    }
+
+</script>
